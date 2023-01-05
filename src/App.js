b69ef7dc2fd6930057f241239b3fbd02bd20117f
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-01-05 12:51:59 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import LikeCounter from "./components/LikeCounter";
 import "./App.css";
-import { Provider } from "react";
 
 function App() {
   return (
-    // <Provider>
     <div className="App">
       <LikeCounter />
     </div>
-    // </Provider>
   );
 }
 
